b6a243cfa0136ad05294aaed55dbcad4366b40a7
phuongpvp <142171291+phuongpvp@users.noreply.github.com>
2025-11-12 08:53:06 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,14 @@
+// Nội dung mới cho file: index.tsx
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ApiKeyProvider } from './ApiKeyManager'; // Import từ file vừa tạo
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApiKeyProvider> {/* Bọc App bằng Provider này */}
+      <App />
+    </ApiKeyProvider>
+  </React.StrictMode>,
 );
